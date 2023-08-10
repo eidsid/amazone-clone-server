e013edbc87e284fbc7c248aa72e7f5b4e33ab940
eidsid <eidsiddiedis@gmail.com>
2023-08-10 10:36:18 +0200

    fix cros problem

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 
 const stripe = require("stripe")(
   "sk_test_51JwvitJSSyNgmcbH3IzDtzpeHeiYCQki89HL3JSC6gwBIN6Edwxd2l11pQhG6TFRUY21FMSKorymjUjgJcDWKHmb004v8Zxbqq"
@@ -8,14 +7,23 @@ const stripe = require("stripe")(
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: true }));
-app.use(express.static(path.join(__dirname, "public")));
-
+app.use(
+  cors({
+    origin: "https://clone-27335.web.app",
+  })
+);
+app.options(
+  "/payments/create",
+  cors({
+    methods: ["POST"],
+    allowedHeaders: ["Authorization"],
+  })
+);
 app.post("/payments/create", async (req, res) => {
   const total = req.body.total;
   if (total > 0) {
     try {
-      console.log("payment request resive Bom!!! for this amount>>>>", total);
+      // console.log("payment request resive Bom!!! for this amount>>>>", total);
       const paymentIntet = await stripe.paymentIntents.create({
         amount: total,
         currency: "usd",
@@ -33,5 +41,7 @@ app.post("/payments/create", async (req, res) => {
 });
 const port = process.env.PORT || 10000;
 app.listen(port, () => {
-  console.log("you are listen on port " + port);
+  if (port === 10000) {
+    console.log("you are listen on port " + port);
+  }
 });
